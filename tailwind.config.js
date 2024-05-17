d8693bd6f8f601d09669fce1e67f00e43763985d
Ndi-Shema <f.shema@alustudent.com>
2024-05-17 12:12:59 +0200

    logo and icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,31 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
+
+    container: {
+      center: false,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "4rem",
+        lg: "6rem",
+        xl: "8rem",
+        "2xl": "12rem",
+        "3xl": "16rem",
+        "4xl": "20rem",
+      },
+      margin: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "4rem",
+        lg: "6rem",
+        xl: "8rem",
+        "2xl": "12rem",
+        "3xl": "16rem",
+        "4xl": "20rem",
+      },
+    },
+
     extend: {},
   },
   plugins: [],
